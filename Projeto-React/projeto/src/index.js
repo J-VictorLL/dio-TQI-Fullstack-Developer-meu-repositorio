c118164ca69ed07f341a2da3d017cc89d85224cd
João Victor <jhonveyne@gmail.com>
2022-06-23 18:57:06 -0300

    refazer

--- Projeto-React/projeto/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GithubProvider from "./providers/GithubProvider";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <GithubProvider />
-  </React.StrictMode>
-, document.getElementById('root'));
